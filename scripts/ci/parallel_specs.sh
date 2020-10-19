85bc44346da92556d325bd72c48002c55f96b6c4
mstruve <mollylbs@gmail.com>
2020-10-19 16:05:43 -0400

    run specs without knapsack

--- scripts/ci/parallel_specs.sh
+++ scripts/ci/parallel_specs.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 cp .env_sample .env
 
-if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
-  KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
-    KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 \
-    KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
-    rake knapsack_pro:rspec # use Regular Mode here always
-else
-  rake knapsack_pro:queue:rspec
-fi
+bundle exec rspec spec/system/user/*.rb
+# if [ "$KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC" = "" ]; then
+#   KNAPSACK_PRO_ENDPOINT=https://api-disabled-for-fork.knapsackpro.com \
+#     KNAPSACK_PRO_MAX_REQUEST_RETRIES=0 \
+#     KNAPSACK_PRO_TEST_SUITE_TOKEN_RSPEC=disabled-for-fork \
+#     rake knapsack_pro:rspec # use Regular Mode here always
+# else
+#   rake knapsack_pro:queue:rspec
+# fi
