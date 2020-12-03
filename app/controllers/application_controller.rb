9007391239a268c1978e3ad4c40fa3bc763bd1eb
benhalpern <bendhalpern@gmail.com>
2020-12-03 08:54:16 -0500

    Get closer to upstream

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -180,7 +180,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :profile_image, :profile_image_url, :education])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username name profile_image profile_image_url])
   end
 
   def internal_nav_param
